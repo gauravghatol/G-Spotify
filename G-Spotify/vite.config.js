bb30f50661396a6a8982c8f362518ada9cc4014a
Gaurav Ghatol <145271523+gauravghatol@users.noreply.github.com>
2025-04-25 14:09:30 +0530

    Update vite.config.js

--- G-Spotify/vite.config.js
+++ G-Spotify/vite.config.js
@@ -1,22 +1,25 @@
 import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    console.log(__dirname);
-    return {
-        define: {
-            __APP_ENV__: env.APP_ENV
-        },
-        root: "src",
-        build: {
-            outDir: "../dist",
-            rollupOptions: {
-                input: {
-                    main: resolve(__dirname, 'src/index.html'),
-                    dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
-                    login: resolve(__dirname, 'src/login/login.html')
-                }
-            }
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __APP_ENV__: env.APP_ENV
+    },
+    root: 'src', // Your entry folder
+    publicDir: '../public', // Optional: use if you have /public assets like icons or logos
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
+          login: resolve(__dirname, 'src/login/login.html'),
         }
+      }
     }
-})
+  }
+});
