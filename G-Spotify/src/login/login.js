4aeec3491ac979a40df83c70e72db883eed43631
Gaurav Ghatol <145271523+gauravghatol@users.noreply.github.com>
2025-04-26 18:11:13 +0530

    Update login.js

--- G-Spotify/src/login/login.js
+++ G-Spotify/src/login/login.js
@@ -1,8 +1,10 @@
 import { ACCESS_TOKEN, EXPIRES_IN, TOKEN_TYPE } from "../common";
 import { APP_URL } from "../config";
 
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const CLIENT_ID = "33ed0c317813438a9a6084c38234fb6f"; // your Spotify Client ID
+const APP_URL = "https://g-spotify.vercel.app"; // your deployed URL
 const REDIRECT_URI = `${APP_URL}/login/login.html`;
+
 const scopes = "user-top-read user-follow-read playlist-read-private user-library-read";
 const authorizeUser = () => {
     const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=${scopes}&show_dialog=true`;
