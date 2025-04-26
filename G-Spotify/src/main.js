b8807de15ece56cbd4f0d2154734a90be3a81966
Gaurav Ghatol <145271523+gauravghatol@users.noreply.github.com>
2025-04-26 18:23:05 +0530

    Update main.js

--- G-Spotify/src/main.js
+++ G-Spotify/src/main.js
@@ -2,8 +2,8 @@ import './style.css';
 import { APP_URL } from "./config";
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem("accessToken")) {
-    window.location.href =  ${APP_URL}/dashboard/dashboard.html;
+    window.location.href =  `${APP_URL}/dashboard/dashboard.html`;
   }else{
-    window.location.href = ${APP_URL}/login/login.html;
+    window.location.href = `${APP_URL}/login/login.html`;
   }
 })
