1f969de214833d346d6759aad039bc64b14b4bcf
Gaurav Ghatol <145271523+gauravghatol@users.noreply.github.com>
2025-04-26 17:59:19 +0530

    Update main.js

--- G-Spotify/src/main.js
+++ G-Spotify/src/main.js
@@ -1,9 +1,11 @@
 import './style.css';
 import { APP_URL } from "./config";
+
 document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem("accessToken")) {
-    window.location.href =  `${APP_URL}/dashboard/dashboard.html`;
-  }else{
-    window.location.href = `${APP_URL}/login/login.html`;
+  const token = localStorage.getItem("ACCESS_TOKEN"); // 🔥 Correct Key Name
+  if (token) {
+    window.location.href = `${APP_URL}/dashboard/dashboard.html`; // redirect to dashboard
+  } else {
+    window.location.href = `${APP_URL}/login/login.html`; // redirect to login
   }
-})
+});
